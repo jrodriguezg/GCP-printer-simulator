8c5fc4a7171c05718c05efd474583bb460ed2897
jrodriguezg <johntherip@gmail.com>
2016-08-08 14:01:59 +0200

    Implement builder pattern on PrintTicket class.

--- GCD-Utils/src/com/jmrodrigg/model/CJT/PrintTicket.java
+++ GCD-Utils/src/com/jmrodrigg/model/CJT/PrintTicket.java
@@ -21,5 +21,97 @@ public class PrintTicket {
     public final ReverseOrder reverse_order;
 
 
-    // TODO: Implement Builder pattern.
+    private PrintTicket(PrintTicketBuilder builder) {
+        this.vendor_ticket_item = builder.vendor_ticket_item;
+        this.color = builder.color;
+        this.duplex = builder.duplex;
+        this.page_orientation = builder.page_orientation;
+        this.copies = builder.copies;
+        this.margins = builder.margins;
+        this.dpi = builder.dpi;
+        this.fit_to_page = builder.fit_to_page;
+        this.page_range = builder.page_range;
+        this.media_size = builder.media_size;
+        this.collate = builder.collate;
+        this.reverse_order = builder.reverse_order;
+    }
+
+    public static class PrintTicketBuilder {
+        private List<Vendor> vendor_ticket_item;
+        private Color color;
+        private Duplex duplex;
+        private PageOrientation page_orientation;
+        private Copies copies;
+        private Margins margins;
+        private Dpi dpi;
+        private FitToPage fit_to_page;
+        private PageRange page_range;
+        private MediaSize media_size;
+        private Collate collate;
+        private ReverseOrder reverse_order;
+
+        public PrintTicketBuilder vendors(List<Vendor> vendor_ticket_item) {
+            this.vendor_ticket_item = vendor_ticket_item;
+            return this;
+        }
+
+        public PrintTicketBuilder color(Color color) {
+            this.color = color;
+            return this;
+        }
+
+        public PrintTicketBuilder duplex(Duplex duplex) {
+            this.duplex = duplex;
+            return this;
+        }
+
+        public PrintTicketBuilder pageOrientation(PageOrientation page_orientation) {
+            this.page_orientation = page_orientation;
+            return this;
+        }
+
+        public PrintTicketBuilder copies(Copies copies) {
+            this.copies = copies;
+            return this;
+        }
+
+        public PrintTicketBuilder margins(Margins margins) {
+            this.margins = margins;
+            return this;
+        }
+
+        public PrintTicketBuilder dpi(Dpi dpi) {
+            this.dpi = dpi;
+            return this;
+        }
+
+        public PrintTicketBuilder fitToPage(FitToPage fit_to_page) {
+            this.fit_to_page = fit_to_page;
+            return this;
+        }
+
+        public PrintTicketBuilder pageRange(PageRange page_range) {
+            this.page_range = page_range;
+            return this;
+        }
+
+        public PrintTicketBuilder mediaSize(MediaSize media_size) {
+            this.media_size = media_size;
+            return this;
+        }
+
+        public PrintTicketBuilder collate(Collate collate) {
+            this.collate = collate;
+            return this;
+        }
+
+        public PrintTicketBuilder reverseOrder(ReverseOrder reverse_order) {
+            this.reverse_order = reverse_order;
+            return this;
+        }
+
+        public PrintTicket build() {
+            return new PrintTicket(this);
+        }
+    }
 }
